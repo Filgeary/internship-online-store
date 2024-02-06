a4446ed532024a4b18e0cecf87e3f6318723a070
Potashov Oleg <potashov_2004@mail.ru>
2024-02-06 13:51:16 +0300

    ts-integration

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 let config = {
   context: path.join(__dirname, '/src'), // Директория с исходным кодом приложения
-  entry: 'index.js', // Главный файл приложения
+  entry: 'index.tsx', // Главный файл приложения
   output: {
     path: path.join(__dirname, 'dist'), // Куда делать оброку
     filename: '[name].js', // Шаблон для названия файлов
@@ -14,7 +14,7 @@ let config = {
   },
   mode: process.env.NODE_ENV,
   resolve: {
-    extensions: ['.js', '.jsx'], // расширения по умолчанию если не указаны в import
+    extensions: ['.tsx', '.ts', '.js', '.jsx'], // расширения по умолчанию если не указаны в import
     modules: ['./', 'node_modules'], // Где искать файлы подключаемых модулей (пакетов)
     alias: {
       '@src': path.resolve(__dirname, './src'),
@@ -27,6 +27,12 @@ let config = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: [{loader: 'babel-loader'}],
+      },
+      // Транспиляция TS/TSX
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{loader: 'ts-loader'}],
       },
       // Правила обработки подключаемых файлов
       {
