2c9ad01232e6cd1186d27493f32c243a8aa9fdda
Georgiy Filippov <g.filippov@ylab.team>
2024-02-16 10:46:07 +0500

    chore(types): add `TRecursiveObjectProps` type

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,30 @@ export type TErrorResponse = {
     data: { issues: any; };
   };
 };
+
+// TODO: show recursive type
+const product = {
+  category: {
+    _id: "123",
+    _type: "category",
+    data: {
+      _id: "456",
+      _type: "sub-category",
+    }
+  },
+  data: {
+    _id: "123",
+    _type: "productData",
+  }
+}
+
+export type TRecursiveObjectProps<T> = {
+  [P in keyof T]: T[P] extends object ? TRecursiveObjectProps<T[P]> : T[P];
+};
+
+// example
+{
+  type TProduct = TRecursiveObjectProps<typeof product>;
+  const fn = (obj: TProduct) => obj.category.data._type
+  fn(product)
+}
